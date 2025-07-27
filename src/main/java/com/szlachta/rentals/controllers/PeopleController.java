0c623f29f22c31175535808c9a55d56f8b339fe2
Krzysztof Szlachta <krzysztof7767@gmail.com>
2025-07-27 13:17:28 +0200

    Add basic endpoints

--- src/main/java/com/szlachta/rentals/controllers/PeopleController.java
+++ src/main/java/com/szlachta/rentals/controllers/PeopleController.java
@@ -1,15 +1,31 @@
 package com.szlachta.rentals.controllers;
 
 import com.szlachta.rentals.dto.PersonRequest;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.szlachta.rentals.dto.PersonResponse;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/person")
 public class PeopleController {
+
+    @GetMapping("/{id}")
+    public PersonResponse getPerson(@PathVariable int id) {
+        return new PersonResponse();
+    }
+
     @PostMapping
-    public void createPerson(PersonRequest personRequest) {
+    public void createPerson(@RequestBody PersonRequest personRequest) {
         System.out.println("test");
     }
+
+    @PutMapping("/{id}")
+    public void updatePerson(@RequestBody PersonRequest personRequest, @PathVariable int id) {
+        System.out.println(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletePerson(@PathVariable int id) {
+        System.out.println(id);
+    }
+
 }
