55e70d050ef0d96e66220ab6cba29142d3febc34
Krzysztof Szlachta <krzysztof7767@gmail.com>
2025-07-27 12:53:10 +0200

    Basic Controller added

--- src/main/java/com/szlachta/rentals/controllers/PeopleController.java
+++ src/main/java/com/szlachta/rentals/controllers/PeopleController.java
@@ -1,8 +1,15 @@
 package com.szlachta.rentals.controllers;
 
-import org.springframework.stereotype.Controller;
+import com.szlachta.rentals.dto.PersonRequest;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
+@RequestMapping("/api/person")
 public class PeopleController {
-
+    @PostMapping
+    public void createPerson(PersonRequest personRequest) {
+        System.out.println("test");
+    }
 }
