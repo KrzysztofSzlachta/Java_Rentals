6b689735bbd9bda2441302c1c2a60a30a8951966
Krzysztof Szlachta <krzysztof7767@gmail.com>
2025-08-15 20:34:22 +0200

    Implemented ReservationsController with basic paths.

--- src/main/java/com/szlachta/rentals/controllers/ReservationsController.java
+++ src/main/java/com/szlachta/rentals/controllers/ReservationsController.java
@@ -1,4 +1,43 @@
 package com.szlachta.rentals.controllers;
 
+import com.szlachta.rentals.dto.ReservationRequest;
+import com.szlachta.rentals.dto.ReservationResponse;
+import com.szlachta.rentals.services.ReservationsService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/reservations")
 public class ReservationsController {
+    private final ReservationsService reservationsService;
+
+    public ReservationsController(ReservationsService reservationsService) {
+        this.reservationsService = reservationsService;
+    }
+
+    @GetMapping("/{id}")
+    public ReservationResponse getReservationById(@PathVariable int id) {
+        return reservationsService.getReservationById(id);
+    }
+
+    @GetMapping
+    public List<ReservationResponse> getAllReservations() {
+        return reservationsService.getAllReservations();
+    }
+
+    @PostMapping
+    public void createReservation(@RequestBody ReservationRequest reservationRequest) {
+        reservationsService.createReservation(reservationRequest);
+    }
+
+    @PutMapping("/{id}")
+    public void updateReservation(@PathVariable int id, @RequestBody ReservationRequest reservationRequest) {
+        reservationsService.updateReservation(reservationRequest, id);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteReservation(@PathVariable int id) {
+        reservationsService.deleteReservation(id);
+    }
 }
